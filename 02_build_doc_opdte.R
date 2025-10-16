914021487cb3c9c4850db184b7d3468ea46764b0
Chen-An Lien <ahn890209@gmail.com>
2025-10-16 17:39:04 +0800

    Refactor script for building doctor-parent weekly panel

--- 02_build_doc_opdte.R
+++ 02_build_doc_opdte.R
@@ -13,11 +13,9 @@
 #' @details
 #' **Inputs**
 #' - `doc_child`: includes columns `ID`, `ID_M`, `ID_F`, `Yr`
-#' - enrollment files (parquet): from western, Chinese, and dental doctors
 #' - outpatient visit files (parquet): same three specialties
 #'
 #' **Outputs**
-#' - `doc_enroll` (data.table): doctor-level monthly panel with birth year, salary
 #' - `doc_opdte` (data.table): doctor-level visit panel with visit year/month
 #' the result is by `ID, FUNC_DATE, HOSP_ID, FUNC_TYPE` to aggregate 
 #' in order to calculate the correct daily presentation
@@ -36,7 +34,6 @@ library(dplyr)
 # Load child-parent linkage --------------------------------------------------
 
 doc_child <- readRDS("data/doc_child.rds")  # contains ID_M, ID_F, ID_C
-doc_child <- doc_child[Yr %in% 2000:2022]
 parent_ids <- unique(c(doc_child$ID_M, doc_child$ID_F))
 
 prsn_info <- open_dataset("E:/H114028/CleanData/processed_data/PersInfo.parquet") %>% 
@@ -48,34 +45,6 @@ prsn_info <- open_dataset("E:/H114028/CleanData/processed_data/PersInfo.parquet"
 prsn_info <- prsn_info[, BIRTH_Y := year(ymd(ID_BIRTHDAY))][, .(ID, ID_S, BIRTH_Y)]
 
 saveRDS(prsn_info, "data/DOC_PRSN_INFO.rds")
-
-# Load enrollment files ------------------------------------------------------
-
-# enroll_dirs <- list(
-#   western = "R Data/doctor-enrol",
-#   chinese = "E:/H113038/liangcheng/data/processed/chinese-doctor-enrol",
-#   dentist = "E:/H113038/liangcheng/data/processed/dentist-enrol"
-# )
-# 
-# enroll_files <- unlist(lapply(enroll_dirs, list.files, full.names = TRUE))
-# 
-# doc_enroll <- rbindlist(
-#   lapply(enroll_files, function(fp) {
-#     open_dataset(fp) %>%
-#       filter(ID %in% parent_ids) %>%
-#       collect() %>%
-#       setDT()[, `:=`(
-#         DATA_YEAR  = year(ym(PREM_YM)),
-#         DATA_MONTH = month(ym(PREM_YM)),
-#         BIRTH_YEAR = year(ymd(ID_BIRTHDAY))
-#       )]
-#   }),
-#   fill = TRUE
-# )
-# 
-# doc_enroll[, ID1_MARK := as.integer(ID == ID1)]
-
-
 
 # Load outpatient visit files ------------------------------------------------
 
@@ -107,7 +76,6 @@ doc_opdte[, FUNC_DATE := ymd(FUNC_DATE)]
 doc_opdte[, MAIN_FUNC_TYPE := FUNC_TYPE[which.max(total_dots)], by = .(PRSN_ID, FUNC_DATE)]
 
 # Save intermediate files if needed
-# save(doc_enroll, file = "R Data/doc_enroll.RData")
 saveRDS(doc_opdte, file = "data/doc_opdte.rds")
 
 # the result is by `ID, FUNC_DATE, HOSP_ID, FUNC_TYPE` to aggregate 
